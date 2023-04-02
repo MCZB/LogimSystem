7b1192eca1af9ff2d4095f3082bddfc78068d217
Mauricio Cezimbra <127701735+MCZB@users.noreply.github.com>
2023-04-02 03:14:42 -0300

    Update login.php

--- login.php
+++ login.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+include 'login_attempts.php';
 
 // Inicialize a variável de mensagem de erro como vazia
 $error_message = "";
@@ -7,29 +8,40 @@ $error_message = "";
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["username"]) && isset($_POST["password"])) {
     require_once "db_connection.php";
 
-    $username = $_POST["username"];
-    $password = $_POST["password"];
+    $username = htmlspecialchars($_POST["username"], ENT_QUOTES, 'UTF-8');
+    $password = htmlspecialchars($_POST["password"], ENT_QUOTES, 'UTF-8');
 
-    $stmt = $conn->prepare("SELECT id, password FROM users WHERE username = ?");
-    $stmt->execute([$username]);
-    $result = $stmt->fetch();
+    if (check_login_attempts($username, $conn)) {
+        $stmt = $conn->prepare("SELECT id, password FROM users WHERE username = ?");
+        $stmt->execute([$username]);
+        $result = $stmt->fetch();
 
-    if ($result) {
-        if (password_verify($password, $result["password"])) {
-            $_SESSION["user_id"] = $result["id"];
-            
-            // Inserir registro de login no banco de dados
-            $login_time = date("Y-m-d H:i:s");
-            $user_ip = $_SERVER["REMOTE_ADDR"];
-            $stmt = $conn->prepare("INSERT INTO login_history (user_id, login_time, user_ip) VALUES (?, ?, ?)");
-            $stmt->execute([$_SESSION["user_id"], $login_time, $user_ip]);
-            
-            header("Location: dashboard.php");
-            exit();
+        if ($result) {
+            if (password_verify($password, $result["password"])) {
+                $_SESSION["user_id"] = $result["id"];
+                
+                // Inserir registro de login no banco de dados
+                $login_time = date("Y-m-d H:i:s");
+                $user_ip = $_SERVER["REMOTE_ADDR"];
+                $stmt = $conn->prepare("INSERT INTO login_history (user_id, login_time, user_ip) VALUES (?, ?, ?)");
+                $stmt->execute([$_SESSION["user_id"], $login_time, $user_ip]);
+                
+                // Registrar tentativa de login bem-sucedida
+                log_login_attempt($username, 1, $conn);
+
+                header("Location: dashboard.php");
+                exit();
+            } else {
+                // Registrar tentativa de login malsucedida
+                log_login_attempt($username, 0, $conn);
+                $error_message = "Usuário ou senha incorretos.";
+            }
+        } else {
+            $error_message = "Usuário ou senha incorretos.";
         }
+    } else {
+        $error_message = "Você excedeu o número máximo de tentativas. Por favor, tente novamente mais tarde.";
     }
-
-    $error_message = "Usuário ou senha incorretos.";
 }
 ?>
 
@@ -55,7 +67,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["username"]) && isset($
         <button type="submit" class="btn btn-primary" name="submit">Entrar</button>
         <?php if ($_SERVER["REQUEST_METHOD"] == "POST" && !empty($error_message)): ?>
             <div class="alert alert-danger mt-4">
-                <?= $error_message ?>
+                <?= htmlspecialchars($error_message, ENT_QUOTES, 'UTF-8') ?>
             </div>
         <?php endif ?>
         <p class="mt-4">Não tem uma conta? <a href="register.php">Cadastre-se</a></p>
