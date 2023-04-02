3367679408dfc4bcdfa562ec95b4dfbad1d7b2f2
Mauricio Cezimbra <127701735+MCZB@users.noreply.github.com>
2023-04-02 16:45:58 -0300

    Update db_connection.php

--- db_connection.php
+++ db_connection.php
@@ -1,29 +1,36 @@
 <?php
-
+// Define as variáveis com as informações de conexão ao banco de dados
 $db_host     = 'localhost';
 $db_name     = 'login_db'; // Substitua pelo nome do seu banco de dados
 $db_user     = 'root'; // Substitua pelo nome de usuário do seu banco de dados
 $db_password = ''; // Substitua pela senha do seu banco de dados
 
+// Tenta criar uma conexão PDO com o banco de dados
 try {
+    // Define a string de conexão DSN com as informações de conexão
     $dsn = "mysql:host=$db_host;dbname=$db_name;charset=utf8";
     $conn = new PDO($dsn, $db_user, $db_password);
+    // Define o modo de erro para exceções
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
+    // Se ocorrer um erro ao conectar, exibe uma mensagem de erro e encerra o script
     die("Erro na conexão: " . $e->getMessage());
 }
 
-// Para parametrizar os dados de entrada e executar uma consulta
+// Prepara a consulta SQL com parâmetros nomeados
 $stmt = $conn->prepare("SELECT * FROM users WHERE username = :username AND password = :password");
+
+// Vincula as variáveis $username e $password aos parâmetros da consulta
 $stmt->bindParam(':username', $username);
 $stmt->bindParam(':password', $password);
 
-// Definir parâmetros e executar a consulta
+// Define os valores das variáveis $username e $password
 $username = "exampleuser";
 $password = "examplepassword";
+
+// Executa a consulta
 $stmt->execute();
 
-// Recupere o resultado
+// Recupera o resultado da consulta
 $result = $stmt->fetch();
-
 ?> 
