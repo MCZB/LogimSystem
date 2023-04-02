a95eb679018ada62825cc984f2d09e73def59340
Mauricio Cezimbra <127701735+MCZB@users.noreply.github.com>
2023-04-02 16:47:34 -0300

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,5 +1,10 @@
 <?php
+// Inicia uma nova sessão ou retoma a sessão existente
 session_start();
+
+// Destrói a sessão atual, removendo todas as variáveis de sessão
 session_destroy();
+
+// Redireciona o usuário para a página de login
 header("Location: login.php");
 ?>
